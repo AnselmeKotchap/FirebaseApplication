c74cecc9df0013b675c8245250b279450dc1e8bc
Anselme <nkanselme@gmail.com>
2017-01-26 11:43:55 +0200

    fake commit

---
+++ GameOfChat/FakeModelCell.swift
@@ -0,0 +1,13 @@
+//
+//  FakeModelCell.swift
+//  GameOfChat
+//
+//  Created by Anselme Kotchap on 2017/01/26.
+//  Copyright © 2017 MIND. All rights reserved.
+//
+
+import UIKit
+
+class FakeModelCell: UICollectionViewCell {
+    //This is a fake model to check that github is working as expected
+}
