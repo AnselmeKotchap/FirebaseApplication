b450ae23c5e3f77de6b0a2e9e57b086bd5940ed3
Anselme <nkanselme@gmail.com>
2017-01-27 10:54:32 +0200

    added a fake testing class

---
+++ GameOfChat/CollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionViewCell.swift
+//  GameOfChat
+//
+//  Created by Anselme Kotchap on 2017/01/27.
+//  Copyright © 2017 MIND. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    //adding a fake class
+    override init(frame: CGRect) {
+       super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
